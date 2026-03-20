12796556b52aac46885feb2e6f8b60c51d53de30
Bilaaaaaaaaal <bbendada24@gmail.com>
2026-03-20 03:21:15 +0000

    Load data and calculate profit and margin columns
    Load product data, drop null values, and create profit and margin columns.

--- SalesAnalysis./main.py
+++ SalesAnalysis./main.py
@@ -1,1 +1,19 @@
+import numpy as np
+from functions import  load_file
+
+"""Loading data first"""
+df = load_file('product_data.csv')
+
+"""Dropping null values"""
+df.dropna(inplace = True)
+
+
+"""Creating new columns that will help us in the process"""
+df['profit'] = df['list_price'] - df['cost_price']
+df['margin'] = df['profit'] / df['list_price'] * 100
+df['cash_cows'] = np.where(df['margin'] > 0, df['margin'], np.nan)
+df['money_pits'] = np.where(df['margin'] < 0, df['margin'], np.nan)
+
+
+
 
